686d0df344d5702021fed2073dd1bcc04db4c7c6
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2024-06-11 20:41:04 -0700

    integrate tailwind and postcss and add try catch in images api

--- server/api/images.py
+++ server/api/images.py
@@ -1,11 +1,14 @@
 from deepface import DeepFace
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 image_routes = Blueprint("image_routes", __name__)
 
 # return the dominant emotion from the image
 @image_routes.route('/', methods=['POST'])
 def analyze_face():
-    image = request.json["image"]
-    result = DeepFace.analyze(image, actions = ['emotion'])
-    sorted_emotions = sorted(result[0]["emotion"].items(),key=lambda x: x[1], reverse=True)
-    return ({sorted_emotions[0][0]: sorted_emotions[0][1]})
+    try:
+        image = request.json["image"]
+        result = DeepFace.analyze(image, actions = ['emotion'])
+        sorted_emotions = sorted(result[0]["emotion"].items(),key=lambda x: x[1], reverse=True)
+        return ({sorted_emotions[0][0]: sorted_emotions[0][1]})
+    except Exception as error:
+        return jsonify({'error': str(error)}), 500
--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,10 +1,10 @@
-import CaptureImage from "../CaptureImage/CaptureImage"
+import CaptureImage from '../CaptureImage/CaptureImage'
 const HomePage: React.FC = () => {
   return (
-    <>
-      <h1>EmoTunes</h1>
+    <div className='bg-emerald-50 h-screen'>
+      <h1 className='text-center text-5xl text-amber'>EmoTunes</h1>
       <CaptureImage />
-    </>
+    </div>
   )
 }
 
