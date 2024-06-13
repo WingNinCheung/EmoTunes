36884a6df104acd3c2d354456f35a1a0a50d5c2c
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2024-06-12 19:54:13 -0700

    adjust /api/images to return two dominant emotions

--- server/api/images.py
+++ server/api/images.py
@@ -2,13 +2,15 @@ from deepface import DeepFace
 from flask import Blueprint, request, jsonify
 image_routes = Blueprint("image_routes", __name__)
 
-# return the dominant emotion from the image
+# return the first two dominant emotion from the image
 @image_routes.route('/', methods=['POST'])
 def analyze_face():
     try:
         image = request.json["image"]
         result = DeepFace.analyze(image, actions = ['emotion'])
         sorted_emotions = sorted(result[0]["emotion"].items(),key=lambda x: x[1], reverse=True)
-        return ({sorted_emotions[0][0]: sorted_emotions[0][1]})
+        first_two_emotions = sorted_emotions[:2]
+        res = {key:value for key,value in first_two_emotions}
+        return res
     except Exception as error:
         return jsonify({'error': str(error)}), 500
