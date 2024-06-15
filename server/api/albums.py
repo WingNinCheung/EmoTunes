c2753d58babd1d2eeca50a08b266ff3625a16a5f
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2024-06-15 14:02:54 -0700

    implement album recommendation with caching

--- server/api/albums.py
+++ server/api/albums.py
@@ -2,40 +2,50 @@ from api.auth import get_spotify_access_token
 from flask import Blueprint
 import requests
 import random
+from api.emotion_range import emotion_range
 album_routes = Blueprint("album_routes", __name__)
+
+emotion_albums_cache = {}
 
 # Get recommended albums based on the specified emotion.
 @album_routes.route('/<emotion>', methods=['GET'])
 def get_albums(emotion):
-    access_token = get_spotify_access_token()
-    emotion_ranges = {
-        "happy": {"valence": (0.7, 1.0), "energy": (0.6, 1.0), "danceability": (0.5, 1.0)},
-        "sad": {"valence": (0.0, 0.4), "energy": (0.3, 0.7), "danceability": (0.2, 0.6)},
-        "angry": {"valence": (0.1, 0.4), "energy": (0.6, 1.0), "danceability": (0.3, 0.7)},
-        "surprise": {"valence": (0.6, 0.9), "energy": (0.5, 0.8), "danceability": (0.5, 0.8)},
-        "fear": {"valence": (0.0, 0.4), "energy": (0.2, 0.6), "danceability": (0.1, 0.5)},
-        "disgust": {"valence": (0.0, 0.3), "energy": (0.2, 0.6), "danceability": (0.1, 0.5)},
-        "neutral": {"valence": (0.4, 0.6), "energy": (0.4, 0.6), "danceability": (0.4, 0.6)},
-    }
-    if emotion not in emotion_ranges:
-        emotion = "neutral"
 
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-    params = {
-        'min_valence': emotion_ranges[emotion]["valence"][0],
-        "max_valence":emotion_ranges[emotion]["valence"][1],
-        "min_energy": emotion_ranges[emotion]["energy"][0],
-        "max_energy": emotion_ranges[emotion]["energy"][1],
-        "min_danceability": emotion_ranges[emotion]["danceability"][0],
-        "max_danceability": emotion_ranges[emotion]["danceability"][1],
-        'seed_genres': 'pop',
-        'limit': 100
-    }
+    tracks = []
 
-    response = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params)
-    tracks = response.json().get('tracks', [])
+    # Check if albums for the emotion are already cached
+    if emotion in emotion_albums_cache:
+        tracks = emotion_albums_cache[emotion]
+    else:
+        # If not cached, fetch albums from Spotify API
+        access_token = get_spotify_access_token()
+
+        if emotion not in emotion_range:
+            emotion = "neutral"
+
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        # Build request parameters based on emotion ranges
+        params = {
+            'min_valence': emotion_range[emotion]["valence"][0],
+            "max_valence":emotion_range[emotion]["valence"][1],
+            "min_energy": emotion_range[emotion]["energy"][0],
+            "max_energy": emotion_range[emotion]["energy"][1],
+            "min_danceability": emotion_range[emotion]["danceability"][0],
+            "max_danceability": emotion_range[emotion]["danceability"][1],
+            'seed_genres': 'pop',
+            'limit': 100
+        }
+        try:
+            response = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params)
+            tracks = response.json().get('tracks', [])
+            emotion_albums_cache[emotion] = tracks
+        except Exception as err:
+            return f"Error fetching tracks from Spotify: {err}"
+        
+    # Randomly select 8 tracks
     selected_tracks = random.sample(tracks, 8)
     albums = [{"album" : track["album"]} for track in selected_tracks]
     return albums
---
+++ server/api/emotion_range.py
@@ -0,0 +1,9 @@
+emotion_range = {
+        "happy": {"valence": (0.7, 1.0), "energy": (0.6, 1.0), "danceability": (0.5, 1.0)},
+        "sad": {"valence": (0.0, 0.4), "energy": (0.3, 0.7), "danceability": (0.2, 0.6)},
+        "angry": {"valence": (0.1, 0.4), "energy": (0.6, 1.0), "danceability": (0.3, 0.7)},
+        "surprise": {"valence": (0.6, 0.9), "energy": (0.5, 0.8), "danceability": (0.5, 0.8)},
+        "fear": {"valence": (0.0, 0.4), "energy": (0.2, 0.6), "danceability": (0.1, 0.5)},
+        "disgust": {"valence": (0.0, 0.3), "energy": (0.2, 0.6), "danceability": (0.1, 0.5)},
+        "neutral": {"valence": (0.4, 0.6), "energy": (0.4, 0.6), "danceability": (0.4, 0.6)},
+    }
